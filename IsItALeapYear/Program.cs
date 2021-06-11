75f10ea3189d70c26fd5ddfc5aa5fcde5daf888e
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-06-10 22:37:42 -0400

    Add files via upload

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IsItALeapYear
+{
+    public  class Program
+    {
+        static void Main()
+        {
+        }
+        public bool IsItALeapYear(int Year)
+        {
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
